9ca2a214b639d6af288e95afe3c3d3428ce50f90
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-04 13:13:14 +0900

    feat: add theme
    #33

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+    mainColor: {
+        activeButton: '#000000',
+        unactiveButton: '#C9C9C9',
+    },
+    color: {
+        white: '#ffffff',
+        black: '#000000',
+        red: '#FF4743',
+        yellow: '#FDC62E',
+        green: '#2EE788',
+        skyblue: '#48CFFA',
+        deepblue: '#4068B0',
+    },
+};
+
+export default theme;
