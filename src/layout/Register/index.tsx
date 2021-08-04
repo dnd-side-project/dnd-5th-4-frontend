0c5387f9c033cf94a17178a0818c82e5b82091ea
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-04 13:15:39 +0900

    feat(registerlayout): add register Layout
    #33

---
+++ src/layout/Register/index.tsx
@@ -0,0 +1,34 @@
+import Button from 'components/Button';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
+import { Container, TopContainer, TitleWraper, Contents, BottomContainer } from './styles';
+
+interface RegisterLayoutProps {
+    children: React.ReactNode;
+    topContents: React.ReactNode;
+    titleContents: React.ReactNode;
+}
+
+const Title: React.FC = ({ children }) => {
+    return <TitleWraper>{children}</TitleWraper>;
+};
+
+const RegisterLayout = ({ children, topContents, titleContents }: RegisterLayoutProps) => {
+    const onClick = () => {};
+
+    return (
+        <Container>
+            <ThemeProvider theme={theme}>
+                <TopContainer>{topContents}</TopContainer>
+                <Title>{titleContents}</Title>
+                <Contents>{children}</Contents>
+                <BottomContainer>
+                    <Button />
+                </BottomContainer>
+            </ThemeProvider>
+        </Container>
+    );
+};
+
+export default RegisterLayout;
