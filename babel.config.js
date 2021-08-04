42633418512895826c788e5d35c27af2bfc24555
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-04 09:33:50 +0900

    feat: add module resolver for baseUrl and custom Url
    #33

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['./src'],
+                    alias: {
+                        Fonts: './assets/fonts',
+                        Images: './assets/images',
+                    },
+                },
+            ],
+        ],
+    };
 };
