b74185b97533ad688c14bde018cf9c00732fdd90
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-10 22:51:14 +0900

    feat(register layout): add register layout
    #33

--- src/layout/Register/index.tsx
+++ src/layout/Register/index.tsx
@@ -1,27 +1,81 @@
 import Button from 'components/Button';
 import React from 'react';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
-import { Container, TopContainer, TitleWraper, Contents, BottomContainer } from './styles';
+import { AntDesign } from '@expo/vector-icons';
+import {
+    Container,
+    TopContainer,
+    TitleContainer,
+    TitleWraper,
+    Contents,
+    BottomContainer,
+    SubTitleWraper,
+    StepBar,
+} from './styles';
 
 interface RegisterLayoutProps {
     children: React.ReactNode;
     topContents: React.ReactNode;
-    titleContents: React.ReactNode;
+    titleContents: React.FC;
+    subTitleContents: React.FC;
+    buttonText: string;
 }
 
 const Title: React.FC = ({ children }) => {
     return <TitleWraper>{children}</TitleWraper>;
 };
+const SubTitle: React.FC = ({ children }) => {
+    return <SubTitleWraper>{children}</SubTitleWraper>;
+};
 
-const RegisterLayout = ({ children, topContents, titleContents }: RegisterLayoutProps) => {
-    const onClick = () => {};
-
+const RegisterLayout = ({
+    children,
+    topContents,
+    titleContents,
+    subTitleContents,
+    buttonText,
+}: RegisterLayoutProps) => {
     return (
         <Container>
             <ThemeProvider theme={theme}>
-                <TopContainer>{topContents}</TopContainer>
-                <Title>{titleContents}</Title>
+                <TopContainer>
+                    {/* {topContents} */}
+                    <TouchableOpacity
+                        onPress={() => {
+                            alert('back button clicked');
+                            // this.props.navigation.goBack();
+                        }}
+                    >
+                        <AntDesign name="left" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            alert('건너뛰기 button clicked');
+                        }}
+                    >
+                        <Text style={{ color: '' }}>건너뛰기</Text>
+                    </TouchableOpacity>
+                </TopContainer>
+                <TitleContainer>
+                    <View style={{ flexDirection: 'column', flex: 1, backgroundColor: 'yellow' }}>
+                        <Title>
+                            {titleContents}
+                            <Text>체질을 기입해주세요</Text>
+                        </Title>
+                        <SubTitle>
+                            {subTitleContents}
+                            <Text>
+                                체질 정보는 옷 추천 기능에서 활용됩니다.
+                                {'\n'}
+                                해당되지 않는다면 건너뛰기를 눌러주세요.
+                            </Text>
+                        </SubTitle>
+                    </View>
+                    <StepBar />
+                </TitleContainer>
+
                 <Contents>{children}</Contents>
                 <BottomContainer>
                     <Button>{buttonText}</Button>
--- src/layout/Register/styles.tsx
+++ src/layout/Register/styles.tsx
@@ -1,10 +1,41 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+    flex: 1;
+    margin: 32px 20px 20px 20px;
 `;
 export const TopContainer = styled.View`
+    height: 64px;
+    background: gray;
+    flex-direction: row;
+    justify-content: space-between;
 `;
+
+export const TitleContainer = styled.View`
+    height: 117px;
+    background-color: #bdbdbd;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
 export const TitleWraper = styled.View`
+    height: 30px;
+    margin-bottom: 14px;
+    background-color: pink;
 `;
-export const Contents = styled.View``;
-export const BottomContainer = styled.View``;
+
+export const SubTitleWraper = styled.View`
+    background: orange;
+`;
+
+export const Contents = styled.View`
+    flex: 1;
+`;
+export const BottomContainer = styled.TouchableHighlight`
+    margin-top: 20px;
+`;
+
+export const StepBar = styled.View`
+    background-color: black;
+    width: 48px;
+`;
