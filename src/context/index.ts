52fa35dec45a743ba7b6d2ea6f716763efbc2dc1
Jeonghye Choi <ppllhm.0@gmail.com>
2021-08-18 14:25:36 +0900

    refactor(context): change context directory structure
    사용하는 context 별로 폴더를 구분
    
    #80

--- src/context/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AuthContext';
-export * from './DressContext';
-export * from './LocationContext';
-export { AuthAction, DressAction } from './types';
-
-export { LocationAction } from './locationTypes';
--- src/context/locationTypes.ts
+++
@@ -1,8 +0,0 @@
-export type LocationAction = {
-    type: 'LOCATION';
-    payload: {
-        location: object;
-    };
-};
-
-export type ChangeLocationAction = LocationAction;
